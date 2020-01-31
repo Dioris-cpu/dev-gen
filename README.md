ecb4e5dbb5144b7dfe745fab5dd9c3a266fa76b1
Dioris Diaz <diorid@gmail.com>
2020-01-30 22:31:42 -0500

    complete maybe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 read me
+ Run npm install
+ 
--- generateHTML.js
+++ generateHTML.js
@@ -1,242 +1,204 @@
 const colors = {
-    green: {
-      wrapperBackground: "#E6E1C3",
-      headerBackground: "#C1C72C",
-      headerColor: "black",
-      photoBorderColor: "#black"
-    },
-    blue: {
-      wrapperBackground: "#5F64D3",
-      headerBackground: "#26175A",
-      headerColor: "white",
-      photoBorderColor: "#73448C"
-    },
-    pink: {
-      wrapperBackground: "#879CDF",
-      headerBackground: "#FF8374",
-      headerColor: "white",
-      photoBorderColor: "#FEE24C"
-    },
-    red: {
-      wrapperBackground: "#DE9967",
-      headerBackground: "#870603",
-      headerColor: "white",
-      photoBorderColor: "white"
-    }
-  };
-  function generateHtml(data){
-      `<!DOCTYPE html>
+  green: {
+    wrapperBackground: "#E6E1C3",
+    headerBackground: "#C1C72C",
+    headerColor: "black",
+    photoBorderColor: "#black"
+  },
+  blue: {
+    wrapperBackground: "#5F64D3",
+    headerBackground: "#26175A",
+    headerColor: "white",
+    photoBorderColor: "#73448C"
+  },
+  pink: {
+    wrapperBackground: "#879CDF",
+    headerBackground: "#FF8374",
+    headerColor: "white",
+    photoBorderColor: "#FEE24C"
+  },
+  red: {
+    wrapperBackground: "#DE9967",
+    headerBackground: "#870603",
+    headerColor: "white",
+    photoBorderColor: "white"
+  }
+};
+const generateHTML = data => {
+  return `       <!DOCTYPE html>
       <html lang="en">
       <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-          <link rel="stylesheet" href="style.css">
-          <title>Document</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <title>Profile Generator</title>
+        <style>
+         .before {
+         box-sizing: border-box;
+         }
+         html, body, .wrapper {
+         height: 100%;
+         }
+         .wrapper {
+         background-color: rgb(179, 172, 172);
+         padding-top: 100px;
+         }
+         body {
+         font-family: 'Vicente Lamónaca', 'Sans', serif;
+         background-color: ${colors[data.color].headerBackground};
+         }
+         main {
+         height: auto;
+         background-color: ${colors[data.color].headerBackground};
+         }
+         .avi {
+         position: center;
+         margin: 0 auto;
+         color: black;
+         padding: 10px;
+         background-color: ${colors[data.color].headerBackground};
+         margin-bottom: -50px;
+         display: flex;
+         justify-content: center;
+         flex-wrap: wrap;
+         width: 95%;
+         border-radius: 6px;
+         }
+         .avi img {
+         width: 250px;
+         height: 250px;
+         border-radius: 50%;
+         object-fit: cover;
+         margin-top: -75px;
+         border: 6px solid black;
+         box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+         }
+         .container {
+         padding: 55px;
+         padding-left: 125px;
+         padding-right: 125px;
+         }
+         h1, h2, h3, h4, h5, h6 {
+         font-family: 'Vicente Lamónaca','Sans', serif;
+         margin: 0;
+         }
+         h1 {
+         font-size: 2.5em;
+         }
+         h2, h3 {
+         font-size: 1.75em;
+         }
+         h4{
+         font-size: 1.5em;
+         }
+         h5, h6 {
+         font-size: 1em;
+         }
+         .avi h1, .avi h2 {
+         width: 100%;
+         text-align: center;
+         }
+         .avi h1 {
+         margin-top: 10px;
+         }
+         .nav-link {
+         display: inline-block;
+         margin: 5px;
+         }
+         .links-nav {
+         width: 100%;
+         text-align: center;
+         font-size: .75em;
+         padding: 0;
+         }
+         .row {
+           display: flex;
+           margin-top: 20px;
+           margin-bottom: 20px;
+         }
+         .col {
+         flex: 1;
+         text-align: center;
+         }
+         .card {
+           color: black;
+           padding: 20px;
+           background-color: ${colors[data.color].headerBackground};
+           margin: 20px;
+           /* gives cards curvy edges */
+           border-radius: 6px;
+         }
+         a, a:hover {
+         text-decoration: none;
+         color: black;
+         font-weight: bolder;
+         }
+      </style>
       </head>
       <body>
-          <div class="wrapper">
-          <div class="photo-header" >
-              <img src="./Images/Dioris1_0917.jpg" alt="Avatar" style="width:200px">
-              <div class="container">
-                <h1 >Hi my name is.....</h1>
-                <h2>Currently at</h2>
-                <div class="links-nav">
-                </div>
-              </div>
+        <!--website wrapper-->
+         <div class="wrapper">
+          <div class="avi">
+        <!--User Profile Picture and heading-->
+          <img src="${data.profilePic}" alt="Profile Picture" />
+          <br>
+          <h1>Hey!</h1>
+          <br>
+          <h2>
+          My name is ${data.name}!</h1>
+          <br>
+          <h5>${data.bio}</h5>
+          <br>
+         <nav class="links-nav">
+           <!--href links to location, repo site, and porfolio-->
+          <a class="nav-link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/${
+            data.location
+          }"><i class="fas fa-location-arrow"></i> Fishtown Philadelphia, PA</a>
+          <a class="nav-link" target="_blank" rel="noopener noreferrer" href="${
+            data.profileUrl
+          }"><i class="fab fa-github-alt"></i> GitHub</a>
+          <a class="nav-link" target="_blank" rel="noopener noreferrer" href="${
+            data.blog
+          }"><i class="fas fa-rss"></i> Blog</a>
+          </nav>
           </div>
-            </div>
+      <!--All content cards will be in this main container-->
+          <main>
+          <div class="container">
           <div class="row">
-              <div class="row">
-                  <div class="col-sm-6">
-                    <div class="card">
-                      <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <div class="card">
-                      <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-sm-6">
-                    <div class="card">
-                      <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <div class="card">
-                      <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                    </div>
-                  </div>
-              </div>
+          <div class="col">
+          <div class="card">
+           <h3>Public Repositories</h3>
+          <h4>${data.repos}</h4>
           </div>
-          <style>
-      
-      @page{
-        margin: 0;
-      }
-      *,
-      *::after,
-      *::before {
-      box-sizing: border-box;
-      }
-      html, body {
-      padding: 0;
-      margin: 0;
-      }
-      html, body, .wrapper {
-      height: 100%;
-      }
-      
-      .wrapper {
-        background-color: red;
-        padding-top: 100px;
-        }
-      
-      
-        body {
-        background-color: white;
-        -webkit-print-color-adjust: exact !important;
-        font-family: 'Cabin', sans-serif;
-        }
-      
-      main {
-        background-color: #E9EDEE;
-        height: auto;
-        padding-top: 30px;
-        }
-        h1, h2, h3, h4, h5, h6 {
-        font-family: 'BioRhyme', serif;
-        margin: 0;
-        }
-        h1 {
-        font-size: 3em;
-        }
-        h2 {
-        font-size: 2.5em;
-        }
-        h3 {
-        font-size: 2em;
-        }
-        h4 {
-        font-size: 1.5em;
-        }
-        h5 {
-        font-size: 1.3em;
-        }
-        h6 {
-        font-size: 1.2em;
-        }
-      
-      .photo-header {
-        position: relative;
-        margin: 0 auto;
-        margin-bottom: -50px;
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-        background-color: white
-      };
-      
-      .photo-header img {
-        width: 250px;
-        height: 250px;
-        border-radius: 50%;
-        border: 6px;
-        object-fit: cover;
-        margin-top: -75px;
-        box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-      
-      }
-      
-      .photo-header h1, .photo-header h2 {
-        width: 100%;
-        text-align: center;
-        }
-        .photo-header h1 {
-        margin-top: 10px;
-        }
-        .links-nav {
-        width: 100%;
-        text-align: center;
-        padding: 20px 0;
-        font-size: 1.1em;
-        }
-      
-        .nav-link {
-          display: inline-block;
-          margin: 5px 10px;
-          }
-          .workExp-date {
-          font-style: italic;
-          font-size: .7em;
-          text-align: right;
-          margin-top: 10px;
-          }
-          .container {
-          padding: 50px;
-          padding-left: 100px;
-          padding-right: 100px;
-          }
-      
-          .row {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            margin-top: 20px;
-            margin-bottom: 20px;
-          }
-      
-          .card {
-            padding: 20px;
-            border-radius: 6px;
-            background-color: blueviolet;
-            color: blue;
-            margin: 20px;
-          }
-      
-      
-          .col {
-            flex: 1;
-            text-align: center;
-            }
-      
-            a, a:hover {
-            text-decoration: none;
-            color: inherit;
-            font-weight: bold;
-            }
-      
-            @media print { 
-             body { 
-               zoom: .75; 
-             } 
-            }
-      
-          
-      
-      
-          </style>
+          </div>
+          <div class="col">
+          <div class="card">
+           <h3>Followers</h3>
+          <h4>${data.followers}</h4>
+          </div>
+          </div>
+           </div>
+          <div class="row">
+          <div class="col">
+          <div class="card">
+           <h3>GitHub Stars</h3>
+          <h4>${data.starCount}</h4>
+           </div>
+            </div>
+          <div class="col">
+           <div class="card">
+            <h3>Following</h3>
+             <h4>${data.following}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      </main>
+      </div>
       </body>
-      </html>`
-  }
-  module.exports = generateHtml;
+      </html>`;
+};
+// generateHTML();
+module.exports = generateHTML;
